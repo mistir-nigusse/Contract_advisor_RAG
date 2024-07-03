73bb26841ce42dffee4f3faf9d47882261591a92
mistirnigusse <mistirnigusse00@gmail.com>
2024-07-03 16:41:49 +0300

    feat: text splitting and embedding to vector db

---
+++ backend/utils/vector_store_util.py
@@ -0,0 +1,15 @@
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+class MyVectorStore:
+    def __init__(self):
+        pass
+
+    def embed_text_and_return_vectorstore(self, text_chunks):
+        embeddings = OpenAIEmbeddings()
+        vectorstore = Chroma.from_texts(text_chunks, embeddings)
+        return vectorstore
+
+    def get_retriever(self, vectorstore):
+        retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+        return retriever
